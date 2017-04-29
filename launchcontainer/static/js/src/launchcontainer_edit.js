9f262dff9a8c0ad8456cfcf5ecf9e574e79a9cf1
Brian Dant <briandant414@gmail.com>
2017-04-29 12:11:38 -0400

    Update the XBlock to use the wharf tokens

--- launchcontainer/static/js/src/launchcontainer_edit.js
+++ launchcontainer/static/js/src/launchcontainer_edit.js
@@ -1,25 +1,38 @@
 function LaunchContainerEditBlock(runtime, element) {
+
     console.log($('.xblock-save-button', element));
+
+    function notify(response) {
+        var $notificationEl = $('#notification-message'); 
+
+        if (response.result === 'success') {
+          $notificationEl.addClass('success');
+          $notificationEl.html('It worked!');
+        }
+        else {
+          $notificationEl.addClass('error');
+          $notificationEl.html("It didn't work! "+response.result); 
+        }
+    }
+
     $('.save-button', element).bind('click', function() {
         var handlerUrl = runtime.handlerUrl(element, 'studio_submit');
         var data = {
             'project': $('input[name=project]').val(),
             'project_friendly': $('input[name=project_friendly]').val(),
+            'project_token': $('input[name=project_token]').val(),
         };
-        runtime.notify('save', {state: 'start'});
-        $.post(handlerUrl, JSON.stringify(data)).done(function(response) {
-            if (response.result === 'success') {
-                runtime.notify('save', {state: 'end'});
-            }
-            else {
-                runtime.notify('save', {state: 'end'});
-                $('#error-message', element).html('Error: '+response.result);
-            }
+
+        $.post(
+            handlerUrl, 
+            JSON.stringify(data))
+          .done(function(response) { 
+              notify(response);
         });
     });
 
     $('.cancel-button', element).bind('click', function() {
-        runtime.notify('cancel', {});
+//        runtime.notify('cancel', {});
     });
 }
 
